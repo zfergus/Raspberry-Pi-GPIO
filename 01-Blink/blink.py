6989dc14cfd06cd8c912ba353e8ac7dc8bdaf194
Zachary Ferguson <zfergus2@users.noreply.github.com>
2015-11-01 22:56:12 -0500

    First Lesson
    Added blink and the readme.

---
+++ 01-Blink/blink.py
@@ -0,0 +1,46 @@
+# blink.py
+# created by Zachary Ferguson
+# A program for the Raspberry Pi that blinks an led.
+
+# Import the necessary files for GPIO use and sleeping the program.
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Constant for the pin the LED is plugged in on.
+LED_PIN = 25
+
+# Setup the GPIO pins for an output on LED_PIN.
+def setup():
+	print "Setting up GPIO"
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(LED_PIN, GPIO.OUT)
+
+# Loop through the blink cycle, Off -> Sleep(2) -> On -> Sleep(2) -> Off.
+# Ends in the off state, but without a sleep after.
+def loop():
+	# LED OFF
+	GPIO.output(LED_PIN, False)
+	print "LED is OFF"
+	sleep(1)
+	
+	# LED ON
+	GPIO.output(LED_PIN, True)
+	print "LED is ON"
+	sleep(1)
+	
+	# LED OFF; NO SLEEP
+	GPIO.output(LED_PIN, False)
+
+# Main function for blink. Sets up then loops then cleans up.
+def main():
+	try:
+		setup()
+		while True:
+			loop()
+	except (KeyboardInterrupt, SystemExit):
+		GPIO.cleanup()
+		raise
+	
+# If being run directly.
+if __name__=="__main__":
+	main()
---
+++ README.md
@@ -0,0 +1,13 @@
+## Raspberry Pi GPIO 
+### Library and Tutorials
+
+This repository contains useful examples and lessons for using the GPIO on the 
+Raspberry Pi. Each lesson contains source files that are designed to be reused 
+in projects and other applications.
+
+Feel free to fork and submit a pull request with additions and modifications.
+
+Submit and issue for direct communication and help.
+
+### Lessons
+1. Blink - blinking an LED. 
